e68ee8d6c582d3fe3f6300f74d882f2f383ac3b3
Wojciech Malota-Wojcik <wojtek@woojciki.pl>
2021-05-26 21:20:17 +0200

    Clean build script a bit

--- build.sh
+++ build.sh
@@ -2,54 +2,47 @@
 
 set -e
 
-TMP=/tmp
-mkdir -p "$TMP"
-
-ISO_EFI=$(mktemp "$TMP"/iso-server-tmp.XXXXXX -d)
-ISO_CONTENT=$(mktemp "$TMP"/iso-server-content.XXXXXX -d)
-INITRAMFS=$(mktemp "$TMP"/iso-server-initramfs.XXXXXX -d)
+DIR=$(mktemp /tmp/iso-server.XXXXXX -d)
 MODULES=/lib/modules/$(uname -r)
 
-dd if=/dev/zero of="$ISO_CONTENT"/efi.img bs=14M seek=1 count=0
-LOOP_DEV=`losetup -f --show "$ISO_CONTENT"/efi.img`
-mkfs.vfat "$LOOP_DEV"
-mount "$LOOP_DEV" "$ISO_EFI"
+mkdir -p "$DIR"/{iso,efi,initramfs/modules,initramfs/etc/pki/tls/certs}
 
-cp /boot/vmlinuz-*.x86_64 "$ISO_EFI"/vmlinuz
+# Copy stuff to initramfs
 
-CGO_ENABLED=0 go build -o "$INITRAMFS"/init ./app
+CGO_ENABLED=0 go build -o "$DIR"/initramfs/init ./app
+cp /etc/pki/tls/certs/ca-bundle.crt "$DIR"/initramfs/etc/pki/tls/certs
+xzcat "$MODULES"/kernel/drivers/block/virtio_blk.ko.xz > "$DIR"/initramfs/modules/virtio_blk.ko
+xzcat "$MODULES"/kernel/drivers/scsi/virtio_scsi.ko.xz > "$DIR"/initramfs/modules/virtio_scsi.ko
+xzcat "$MODULES"/kernel/drivers/net/virtio_net.ko.xz > "$DIR"/initramfs/modules/virtio_net.ko
+xzcat "$MODULES"/kernel/drivers/net/net_failover.ko.xz > "$DIR"/initramfs/modules/net_failover.ko
+xzcat "$MODULES"/kernel/net/core/failover.ko.xz > "$DIR"/initramfs/modules/failover.ko
 
-mkdir -p "$INITRAMFS"/{modules,etc/pki/tls/certs}
+# Copy stuff to EFI
 
-xzcat "$MODULES"/kernel/drivers/block/virtio_blk.ko.xz > "$INITRAMFS"/modules/virtio_blk.ko
-xzcat "$MODULES"/kernel/drivers/scsi/virtio_scsi.ko.xz > "$INITRAMFS"/modules/virtio_scsi.ko
-xzcat "$MODULES"/kernel/drivers/net/virtio_net.ko.xz > "$INITRAMFS"/modules/virtio_net.ko
-xzcat "$MODULES"/kernel/drivers/net/net_failover.ko.xz > "$INITRAMFS"/modules/net_failover.ko
-xzcat "$MODULES"/kernel/net/core/failover.ko.xz > "$INITRAMFS"/modules/failover.ko
+dd if=/dev/zero of="$DIR"/iso/efi.img bs=14M seek=1 count=0
+mkfs.vfat "$DIR"/iso/efi.img
+mount "$DIR"/iso/efi.img "$DIR"/efi
 
-cp /etc/pki/tls/certs/ca-bundle.crt "$INITRAMFS"/etc/pki/tls/certs
+cp /boot/vmlinuz-*.x86_64 "$DIR"/efi/vmlinuz
 
-pushd "$INITRAMFS"
-find . | cpio -c -o --owner root:root | xz --check=crc32 > "$ISO_EFI"/initramfs.img
+pushd "$DIR"/initramfs
+find . | cpio -c -o --owner root:root | xz --check=crc32 > "$DIR"/efi/initramfs.img
 popd
 
-# startup.nsh is used as a fallback if no valid UEFi entry is found
-echo "vmlinuz loglevel=4 initrd=\initramfs.img" > "$ISO_EFI"/startup.nsh
+# startup.nsh is used as a fallback if no valid UEFI entry is found
+echo "vmlinuz loglevel=4 initrd=\initramfs.img" > "$DIR"/efi/startup.nsh
 
-umount "$ISO_EFI"
-losetup -d "$LOOP_DEV"
+umount "$DIR"/efi
 
-TIME=`date +"%Y-%m-%d-%H-%M-%S"`
-ISO_OUT=./server-"$TIME".iso
+# Build ISO image
 
 xorriso -as mkisofs \
-  -iso-level 3 \
-  -r -V "os" \
+  -iso-level 3 -r \
   -J -joliet-long \
   -no-emul-boot \
   -e /efi.img \
   -partition_cyl_align all \
-  -o "$ISO_OUT" \
-  "$ISO_CONTENT"
+  -o ./server-$(date +"%Y-%m-%d-%H-%M-%S").iso \
+  "$DIR"/iso
 
-rm -rf "$ISO_EFI" "$ISO_CONTENT" "$INITRAMFS"
+rm -rf "$DIR"

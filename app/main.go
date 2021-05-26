60ebcc69d4c49fee697b896de7ae8e6aa108b5ee
Wojciech Malota-Wojcik <wojtek@woojciki.pl>
2021-05-26 21:51:08 +0200

    Move tinyboot os configuration to reusable library

--- app/main.go
+++ app/main.go
@@ -1,96 +1,22 @@
 package main
 
 import (
-	"context"
 	"fmt"
 	"io"
 	"io/ioutil"
 	"log"
-	"net"
 	"net/http"
 	"os"
-	"sync"
 	"time"
 
-	"github.com/digineo/go-dhclient"
-	"github.com/google/gopacket/layers"
-	"github.com/milosgajdos/tenus"
-	"golang.org/x/sys/unix"
+	"github.com/wojciech-malota-wojcik/tinyboot"
 )
-
-var dhcpRequestList = []layers.DHCPOpt{
-	layers.DHCPOptSubnetMask,
-	layers.DHCPOptRouter,
-}
 
 func main() {
 	// ignore logs produced by dhclient
 	log.SetOutput(ioutil.Discard)
 
-	fmt.Println("Loading kernel modules")
-	ok(modprobe("/modules/failover.ko"))
-	ok(modprobe("/modules/net_failover.ko"))
-	ok(modprobe("/modules/virtio_net.ko"))
-	ok(modprobe("/modules/virtio_scsi.ko"))
-	ok(modprobe("/modules/virtio_blk.ko"))
-
-	fmt.Println("Network interfaces:")
-	ifs, err := net.Interfaces()
-	ok(err)
-
-	wait := sync.WaitGroup{}
-	for _, iface := range ifs {
-		name := iface.Name
-		fmt.Println(name)
-		if iface.Flags&net.FlagLoopback != 0 {
-			// loopback interface
-			continue
-		}
-
-		wait.Add(1)
-
-		link, err := tenus.NewLinkFrom(name)
-		ok(err)
-		if iface.Flags&net.FlagUp == 0 {
-			fmt.Printf("Interface %s is down, starting it\n", name)
-			ok(link.SetLinkUp())
-		}
-		fmt.Printf("Setting DHCP for interface %s\n", name)
-		client := dhclient.Client{
-			Iface: &iface,
-			OnBound: func(lease *dhclient.Lease) {
-				ok(link.SetLinkIp(lease.FixedAddress, &net.IPNet{IP: lease.FixedAddress, Mask: lease.Netmask}))
-				ok(link.SetLinkDefaultGw(&lease.Router[0]))
-				fmt.Printf("Interface %s configured:\n%+v\n", name, lease)
-
-				wait.Done()
-			},
-		}
-
-		for _, param := range dhcpRequestList {
-			client.AddParamRequest(param)
-		}
-
-		client.Start()
-		defer client.Stop()
-	}
-	wait.Wait()
-
-	dialer := &net.Dialer{
-		Resolver: &net.Resolver{
-			PreferGo: true,
-			Dial: func(ctx context.Context, network, address string) (net.Conn, error) {
-				d := net.Dialer{
-					Timeout: 5 * time.Second,
-				}
-				return d.DialContext(ctx, "udp", "8.8.8.8:53")
-			},
-		},
-	}
-
-	http.DefaultTransport.(*http.Transport).DialContext = func(ctx context.Context, network, addr string) (net.Conn, error) {
-		return dialer.DialContext(ctx, network, addr)
-	}
+	tinyboot.Configure()
 
 	for {
 		func() {
@@ -100,25 +26,11 @@ func main() {
 			} else {
 				defer resp.Body.Close()
 				_, err = io.Copy(os.Stdout, resp.Body)
-				ok(err)
+				if err != nil {
+					panic(err)
+				}
 			}
 		}()
 		<-time.After(time.Second)
-	}
-}
-
-func modprobe(file string) error {
-	f, err := os.Open(file)
-	if err != nil {
-		return err
-	}
-	defer f.Close()
-
-	return unix.FinitModule(int(f.Fd()), "", 0)
-}
-
-func ok(err error) {
-	if err != nil {
-		panic(err)
 	}
 }
---
+++ configure.go
@@ -0,0 +1,111 @@
+package tinyboot
+
+import (
+	"context"
+	"fmt"
+	"net"
+	"net/http"
+	"os"
+	"sync"
+	"time"
+
+	"github.com/digineo/go-dhclient"
+	"github.com/google/gopacket/layers"
+	"github.com/milosgajdos/tenus"
+	"golang.org/x/sys/unix"
+)
+
+var dhcpRequestList = []layers.DHCPOpt{
+	layers.DHCPOptSubnetMask,
+	layers.DHCPOptRouter,
+}
+
+// Configure configures tinyboot os
+func Configure() {
+	if os.Args[0] != "/init" {
+		// We are not inside tinyboot os so there is nothing to configure
+		return
+	}
+
+	// Load kernel modules for qemu hardware
+	modprobe("/modules/failover.ko")
+	modprobe("/modules/net_failover.ko")
+	modprobe("/modules/virtio_net.ko")
+	modprobe("/modules/virtio_scsi.ko")
+	modprobe("/modules/virtio_blk.ko")
+
+	// Configure network
+	ifs, err := net.Interfaces()
+	ok(err)
+
+	wait := sync.WaitGroup{}
+	for _, iface := range ifs {
+		name := iface.Name
+		fmt.Println(name)
+		if iface.Flags&net.FlagLoopback != 0 {
+			// loopback interface
+			continue
+		}
+
+		wait.Add(1)
+
+		link, err := tenus.NewLinkFrom(name)
+		ok(err)
+		if iface.Flags&net.FlagUp == 0 {
+			fmt.Printf("Interface %s is down, starting it\n", name)
+			ok(link.SetLinkUp())
+		}
+		fmt.Printf("Setting DHCP for interface %s\n", name)
+		client := dhclient.Client{
+			Iface: &iface,
+			OnBound: func(lease *dhclient.Lease) {
+				ok(link.SetLinkIp(lease.FixedAddress, &net.IPNet{IP: lease.FixedAddress, Mask: lease.Netmask}))
+				ok(link.SetLinkDefaultGw(&lease.Router[0]))
+				fmt.Printf("Interface %s configured:\n%+v\n", name, lease)
+
+				wait.Done()
+			},
+		}
+
+		for _, param := range dhcpRequestList {
+			client.AddParamRequest(param)
+		}
+
+		client.Start()
+		defer client.Stop()
+	}
+	wait.Wait()
+
+	// Configure DNS resolver
+	dialer := &net.Dialer{
+		Resolver: &net.Resolver{
+			PreferGo: true,
+			Dial: func(ctx context.Context, network, address string) (net.Conn, error) {
+				d := net.Dialer{
+					Timeout: 5 * time.Second,
+				}
+				return d.DialContext(ctx, "udp", "8.8.8.8:53")
+			},
+		},
+	}
+
+	http.DefaultTransport.(*http.Transport).DialContext = func(ctx context.Context, network, addr string) (net.Conn, error) {
+		return dialer.DialContext(ctx, network, addr)
+	}
+}
+
+func modprobe(file string) {
+	f, err := os.Open(file)
+	ok(err)
+	defer func() {
+		ok(f.Close())
+	}()
+
+	ok(unix.FinitModule(int(f.Fd()), "", 0))
+}
+
+func ok(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
